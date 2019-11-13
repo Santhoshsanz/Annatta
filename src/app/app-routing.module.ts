53f160729887ddb94f26a74cd5b03e8901a56c70
Santhosh Kumar <santhoshkumar@b-one.com>
2019-11-13 20:58:40 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductDetailComponent } from 'src/app/module/product-detail/product-detail/product-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  loadChildren: () => import('./module/product-detail/product-detail.module').then(m => m.ProductDetailModule)
+  // component: ProductDetailComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
